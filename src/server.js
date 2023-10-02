dabf2d5bfad9bb1e74fedc1901f4d47a35a27d04
Moataz Mohammady <moataz@wuilt.com>
2023-10-03 01:02:36 +0300

    fix logout

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { Client, LocalAuth } = require("whatsapp-web.js");
+const { Client, LocalAuth, LegacySessionAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 // const https = require("https");
 const cors = require('cors');
-// const fs = require('fs');
+const fs = require('fs');
 
 const app = express();
 let qrCodeData = "";
@@ -24,15 +24,41 @@ app.use(cors());
 
 
 // Start the client
+// const client = new Client({
+//   authStrategy: new LocalAuth(),
+//   restartOnAuthFail: true, // related problem solution
+//   puppeteer: {
+//     headless: true,
+//     args: ['--no-sandbox']
+//   }
+// });
+
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
+
+// Load the session data if it has been previously saved
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  restartOnAuthFail: true, // related problem solution
-  puppeteer: {
-    headless: true,
-    args: ['--no-sandbox']
-  }
+  authStrategy: new LegacySessionAuth({
+    session: sessionData
+  })
 });
 
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
 
 
 
