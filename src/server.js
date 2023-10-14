cf0a90ae3ca6b46534b5a46335e04a36641dd0ed
Moataz Mohammady <moataz@wuilt.com>
2023-10-14 11:55:42 +0300

    puppeteer

--- src/server.js
+++ src/server.js
@@ -33,7 +33,13 @@ const client = new Client({
   restartOnAuthFail: true, // related problem solution
   puppeteer: {
     headless: true,
-    args: ['--no-sandbox']
+    args: ['--no-sandbox', '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--disable-gpu']
   }
 });
 
