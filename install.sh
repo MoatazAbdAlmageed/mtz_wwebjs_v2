f791582698391aa6d14e24ce7f3a1c26573d69bd
Moataz Mohammady <moataz@wuilt.com>
2025-06-04 04:32:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+# 2. Update and upgrade the system
+sudo apt update && sudo apt upgrade -y
+
+# 3. Install curl
+sudo apt install curl -y
+
+# 4. Download and run NodeSource LTS setup script
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+
+# 5. Install Node.js and npm
+sudo apt install -y nodejs
+
+# 6. Verify installation
+node -v
+npm -v
+
+
 sudo apt-get install chromium-browser
 sudo apt update
 sudo apt install -y \
@@ -5,3 +22,6 @@ sudo apt install -y \
   libexpat1 libfontconfig1 libgbm1 libglib2.0-0t64 libgtk-3-0t64 libnspr4 libnss3 libpango-1.0-0 \
   libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 \
   libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation lsb-release xdg-utils wget
+
+
+sudo apt install nginx -y
